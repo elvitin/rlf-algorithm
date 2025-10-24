dd032a55a8b2448910d6e2762de0219fff60339a
Victor Rodrigues <victortavi@outlook.com>
2025-10-23 21:49:29 -0300

    refactor: update VSCode settings and enhance index.ts for better readability

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
 import fs from 'node:fs';
 import { EOL } from 'node:os';
+
 console.info('Coloração Grafos...');
 
+//read content (List of List)
 const content = fs.readFileSync('./docs/ListaTesteAColoracao.txt', { encoding: 'utf-8' });
 const lines = content.split(EOL);
 console.info({ lines });
+
+//remove first lines of vertices
+lines.shift();
+
+//get list of list (v1)
+const listOfList = lines.map(line => line.split(' '));
+console.info({ listOfList });
+
+//get list of list (v2)
+const listOfListV2 = lines.reduce((prev, curr) => {
+	const elements = curr.split(' ');
+	const firstElement = elements.shift() as string;
+	Reflect.set(prev, firstElement, elements);
+	return prev;
+}, {});
+console.info({ listOfListV2 });
